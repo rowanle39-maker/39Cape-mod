8eaa0b2d2d9261f844ae482de3616f84b1bb17be
rowanle39-maker <rowanle39@gmail.com>
2026-07-08 11:26:59 +0300

    Create Recipes.java

---
+++ src/main/java/com/cape39/spigotcape/util/Recipes.java
@@ -0,0 +1,51 @@
+package com.cape39.spigotcape.util;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipes {
+
+    public static final NamespacedKey FRAME_RECIPE_KEY = new NamespacedKey("cape39", "cape_frame");
+    public static final NamespacedKey CAPE_RECIPE_KEY = new NamespacedKey("cape39", "banner_cape");
+
+    public static void registerAll(JavaPlugin plugin) {
+        registerFrameRecipe(plugin);
+        registerCapeRecipe(plugin);
+    }
+
+    private static void registerFrameRecipe(JavaPlugin plugin) {
+        ShapedRecipe recipe = new ShapedRecipe(FRAME_RECIPE_KEY, ItemFactory.createFrame());
+        recipe.shape("ISI");
+        recipe.setIngredient('I', Material.IRON_INGOT);
+        recipe.setIngredient('S', Material.STICK);
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private static void registerCapeRecipe(JavaPlugin plugin) {
+        ShapelessRecipe recipe = new ShapelessRecipe(CAPE_RECIPE_KEY,
+                ItemFactory.createCapeFromBanner(new ItemStack(Material.WHITE_BANNER)));
+
+        recipe.addIngredient(new RecipeChoice.ExactChoice(ItemFactory.createFrame()));
+        recipe.addIngredient(allBannersChoice());
+
+        plugin.getServer().addRecipe(recipe);
+    }
+
+    private static RecipeChoice allBannersChoice() {
+        List<ItemStack> banners = new ArrayList<>();
+        for (Material m : Material.values()) {
+            if (m.name().endsWith("_BANNER")) {
+                banners.add(new ItemStack(m));
+            }
+        }
+        return new RecipeChoice.ExactChoice(banners);
+    }
+}
