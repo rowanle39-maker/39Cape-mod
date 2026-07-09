dc47d5a8630c4536181cf51493df2e497d99e8df
rowanle39-maker <rowanle39@gmail.com>
2026-07-09 11:31:33 +0300

    Update CapePlugin.java

--- src/main/java/com/cape39/spigotcape/CapePlugin.java
+++ src/main/java/com/cape39/spigotcape/CapePlugin.java
@@ -1,20 +1,30 @@
 package com.cape39.spigotcape;
 
+import com.cape39.spigotcape.cape.CapeDisplayManager;
 import com.cape39.spigotcape.listeners.CraftListener;
 import com.cape39.spigotcape.util.Recipes;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CapePlugin extends JavaPlugin {
 
+    private CapeDisplayManager capeDisplayManager;
+
     @Override
     public void onEnable() {
         Recipes.registerAll(this);
         getServer().getPluginManager().registerEvents(new CraftListener(this), this);
+
+        capeDisplayManager = new CapeDisplayManager(this);
+        capeDisplayManager.start();
+
         getLogger().info("Cape39 aktif edildi.");
     }
 
     @Override
     public void onDisable() {
+        if (capeDisplayManager != null) {
+            capeDisplayManager.stop();
+        }
         getLogger().info("Cape39 devre dışı bırakıldı.");
     }
 }
